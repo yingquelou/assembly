bc36486a57c68aaf1e2a940d26cad051b667872e
yingquelou <2695193723@qq.com>
2022-05-26 00:52:58 +0800

    汇编模块化程序设计

---
+++ test/hello.asm
@@ -0,0 +1,20 @@
+; a simple hello masm sample
+;EXPECT-stdout: "hello masm"
+
+.586
+DATA SEGMENT USE16
+	MESG DB 'hello masm',0AH,'$'
+DATA ENDS
+CODE SEGMENT USE16
+	     ASSUME CS:CODE,DS:DATA
+	BEG: MOV    AX,DATA
+	     MOV    DS, AX
+	     MOV    CX,8
+	LAST:MOV    AH,9
+	     MOV    DX, OFFSET MESG
+	     INT    21H
+	     LOOP   LAST
+	     MOV    AH,4CH
+	     INT    21H            	;BACK TO DOS
+CODE ENDS
+END  BEG
