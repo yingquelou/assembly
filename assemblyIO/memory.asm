66570b025fd5ed6243464c755f2805939af7f1fd
yingquelou <2695193723@qq.com>
2022-05-30 23:45:12 +0800

    内存拷贝

--- assemblyIO/memory.asm
+++ assemblyIO/memory.asm
@@ -1,34 +1,40 @@
 .8086
-assume cs:codesg,ss:stacksg
+assume cs:codesg,ss:stacksg,ds:string
 stacksg segment
-            dw 6 dup(0)
+            word 15 dup(0)
 stacksg ends
+string segment
+           byte 256 dup('A',0)
+string ends
 
-codesg segment
-    start:        
-                  mov  ax,stacksg
-                  mov  ss,ax
-                  mov  sp,12
-                  mov  dh,20                    ;开始传参
-                  mov  dl,0
-                  mov  bh,' '
-                  mov  bl,0ffh
+codesg segment use16
+    start:        mov   ax,stacksg
+                  mov   ss,ax
+                  mov   sp,30
+
+                  mov   ax,string
+                  mov   ds,ax
+
+                  mov   bx,256*2-1
+                  mov   cx,0ffh
+
+    lp:           mov   [bx],cl
+                  sub   bx,2
+                  loop  lp
+
+                  mov   ax,0b800h
+                  mov   es,ax
+                  mov   di,990
+                  xor   si,si
+                  mov   cx,(0ffh+1)*2
+
+                  call  far ptr memmove
 
 
-                  mov  cx,80
-              
-    lp:           push cx
-                  mov  cx,bx                    ;传参完成
-                  mov  ax,dx
-                  call far ptr show_char
-                  dec  bl
-                  inc  dl
-                  pop  cx
-                  loop lp
 
 
-    last:         mov  ax,4c00h
-                  int  21h
+                  mov   ax,4c00h
+                  int   21h
 
 
 
@@ -41,25 +47,25 @@ codesg segment
     ;这可能有用,所以返回值是:ax,cx
     ;注意:栈至少预留4word的空间
     ;函数缺乏泛用性：待泛函完成后再作修改——类似c语言的memcpy/memmove,注意空间重叠问题
-                  push ds
-                  push bx                       ;保存寄存器环境
+                  push  ds
+                  push  bx                       ;保存寄存器环境
 
-                  mov  bx,0b800h                ;正文开始
-                  mov  ds,bx                    ;让ds指向显存起始段
+                  mov   bx,0b800h                ;正文开始
+                  mov   ds,bx                    ;让ds指向显存起始段
 
-                  mov  bl,al                    ;开始找显示的位置
-                  mov  al,160
-                  mul  ah
-                  add  bl,bl
-                  xor  bh,bh
-                  add  ax,bx
-                  mov  bx,ax                    ;找到了,地址在bx里
+                  mov   bl,al                    ;开始找显示的位置
+                  mov   al,160
+                  mul   ah
+                  add   bl,bl
+                  xor   bh,bh
+                  add   ax,bx
+                  mov   bx,ax                    ;找到了,地址在bx里
 
-                  mov  [bx],ch                  ;字符入显存
-                  mov  [bx].1,cl                ;样式入显存
+                  mov   [bx],ch                  ;字符入显存
+                  mov   [bx].1,cl                ;样式入显存
     ;已用寄存器:ax,bx,cx,ds,其中用作参数的寄存器是ax、cx,其余寄存器要作合适的栈处理
-                  pop  bx
-                  pop  ds
+                  pop   bx
+                  pop   ds
                   retf
    
     
@@ -67,32 +73,59 @@ codesg segment
     ; entry:(ax)=EA,(dx)=SA
     ; return:(ax)=物理地址低位,(dx)=物理地址高位
     ; 栈空间预留1word
-                  push si
+                  pushf
+                  push  si
 
-                  mov  si,dx
-                  mov  dx,16
-                  mul  dx
-                  add  ax,si
-                  adc  dx,0
+                  mov   si,ax
+                  mov   ax,16
+                  mul   dx
+                  add   ax,si
+                  adc   dx,0
 
-                  pop  si
+                  pop   si
+                  popf
                   retf
     memmove:      
     ;从指定内存读取给定长度的数据到另一个给定内存(源空间与目的空间有重叠可能)
-                  jmp  j
-                  tmp  dword ?,?
+    ; entry:source——ds:si,dest——es:di,length——cx
+                  pushf
+                  push  ax
+                  push  dx
+                  
+                  xor   ax,ax
+                  push  ax
+                  popf
+                  jmp   j
+                  tmp   word 2 dup(?)
 
-    j:            mov  ax,di
-                  mov  dx,es
-                  call far ptr getRealAdress
-                  mov  word ptr tmp[0],ax
-                  mov  word ptr tmp[1],dx
-                  mov  ax,si
-                  mov  dx,ds
-                  call far ptr getRealAdress
-                  mov  word ptr tmp[2],ax
-                  mov  word ptr tmp[3],dx
-                ;   cmp  tmp[0],tmp[2]
+    j:            mov   ax,di
+                  mov   dx,es
+                  call  far ptr getRealAdress
+                  mov   tmp[0],ax
+                  mov   tmp[2],dx
+                  mov   ax,si
+                  mov   dx,ds
+                  call  far ptr getRealAdress
+                 
+                  cmp   dx,tmp[2]                ;比较高位
+                  ja    cp
+                  jne   recp
+
+                  cmp   ax,tmp[0]                ;比较低位
+                  ja    cp
+                  jne   recp
+                  jmp   rt
+
+    recp:         std
+                  dec   cx
+                  add   si,cx                    ;是否有进位？
+                  add   di,cx                    ;是否有进位？
+                  inc   cx
+    cp:           rep   movsb                    ;复制
+    rt:           
+                  pop   dx
+                  pop   ax
+                  popf
                   retf
     show_str:     
     ;功能——在指定的位置,以指定的颜色显示一个以0结尾的字符串
